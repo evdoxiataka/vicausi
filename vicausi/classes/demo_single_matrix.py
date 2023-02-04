0009a982a92cf918605e31934f61fb9310338f34
evdoxiataka <evitaka@hotmail.com>
2023-02-04 18:16:56 +0000

    adds getters for presentation of the examples in the introduction of user study

--- vicausi/classes/demo_single_matrix.py
+++ vicausi/classes/demo_single_matrix.py
@@ -46,9 +46,11 @@ class Demo_Single_Matrix():
             self.single_intervention = False
         ##
         self.plot = None
-        self.dag_plots = None
+        self.dag_objs = None
         self.data = None
-        self.widget = None
+        self.widget_obj = None
+        self.grid_obj = None
+        self.dags_col = None
         ##
         self.initialize_plot()
 
@@ -63,7 +65,7 @@ class Demo_Single_Matrix():
         self.data = Data(self.files)       
         ## Create DAGs   
         causal_dags_ids = self.data.get_causal_dags_ids()
-        causal_dags_obj = [Causal_DAG(self.data, dag_id, self.status, self.var_order) for dag_id in causal_dags_ids]
+        self.dag_objs = [Causal_DAG(self.data, dag_id, self.status, self.var_order) for dag_id in causal_dags_ids]
         # causal_dags_obj = [Causal_DAG(self.data, dag_id, self.var_order) for dag_id in self.dags_presented]
         ## Markdowns
         t_graphs = pn.pane.Markdown('''## Simulated Data of an Unidentified Causal Model''')
@@ -74,45 +76,45 @@ class Demo_Single_Matrix():
             t_radio = pn.pane.Markdown('''*Select an intervention to see the simulated data of the intervention.*''', style=self.style)
         ## Create Widget object
         interv_data = self.data.get_interventions(self.dag_id) ## Dict (<itype>: Dict(<var>:List/numpy array of samples))
-        self.widget = Widget_Single_Matrix(self.status, self.action_vars, {itype:interv_data[itype] for itype in self.action_vars if itype in interv_data})
+        self.widget_obj = Widget_Single_Matrix(self.status, self.action_vars, {itype:interv_data[itype] for itype in self.action_vars if itype in interv_data})
         ## Create Scatter Matrix
-        grid_obs = Scatter_Matrix(self.data, self.dag_id, self.var_order, self.status, self.showData)
+        self.grid_obj = Scatter_Matrix(self.data, self.dag_id, self.var_order, self.status, self.showData)
         ##
-        self.widget.register_callbacks_to_dags(causal_dags_obj)
-        self.widget.register_callbacks_to_cells([grid_obs])
+        self.widget_obj.register_callbacks_to_dags(self.dag_objs)
+        self.widget_obj.register_callbacks_to_cells([self.grid_obj])
         ## DIAGRAMS - FIGURES 
         ## DAGS col
-        dags_cols = [pn.Column(pn.pane.Bokeh(causal_dags_obj[i].get_plot())) for i,_ in enumerate(causal_dags_obj)]
-        dags_col = pn.Column(t_dags,*dags_cols)
+        dags_cols = [pn.Column(pn.pane.Bokeh(self.dag_objs[i].get_plot())) for i,_ in enumerate(self.dag_objs)]
+        self.dags_col = pn.Column(t_dags,*dags_cols)
         ## GRID col
         if self.status not in ["static"]:
-            grids_col = pn.Column(pn.Column(self.widget.slider, css_classes=['panel-widget-box'], width = self.width), grid_obs.get_grid())
+            grids_col = pn.Column(pn.Column(self.widget_obj.slider, css_classes=['panel-widget-box'], width = self.width), self.grid_obj.get_grid())
         else:
-            grids_col = pn.Row(grid_obs.get_grid(),pn.pane.Bokeh(grid_obs.cells[self.var_order[-1]][-1].plot_colorbar))
+            grids_col = pn.Row(self.grid_obj.get_grid(),pn.pane.Bokeh(self.grid_obj.cells[self.var_order[-1]][-1].plot_colorbar))
         ## WIDGET BOX col
         if (self.single_intervention and self.status == "animated") or (self.single_intervention == False):
-            widget_boxes = self.widget.get_widget_box()
+            widget_boxes = self.widget_obj.get_widget_box()
             widget_col = pn.Column(t_radio,*widget_boxes, css_classes=['panel-widget-box'])
             widget_row = pn.Column(t_radio, pn.Row(*widget_boxes, css_classes=['panel-widget-box']))
         if self.single_intervention:            
             ## PLOT
             if self.status not in ["animated"]:  
-                self._activate_radio_button_if_single_inter(self.widget)   
-                self.plot = pn.Column(t_graphs, pn.Row(pn.Column(grids_col, self.widget.toggle3), dags_col))                           
-                # self.plot = pn.Row(pn.Column(t_graphs, grids_col, self.widget.toggle3), dags_col)
+                self._activate_radio_button_if_single_inter(self.widget_obj)   
+                self.plot = pn.Column(t_graphs, pn.Row(pn.Column(grids_col, self.widget_obj.toggle3), self.dags_col))                           
+                # self.plot = pn.Row(pn.Column(t_graphs, grids_col, self.widget.toggle3), self.dags_col)
             else:
                 # widget_boxes = self.widget.get_widget_box() 
-                self.plot = pn.Column(t_graphs,widget_row, pn.Row(grids_col, dags_col))
-                # self.plot = pn.Row(pn.Column(t_graphs,widget_col, grids_col), dags_col)
+                self.plot = pn.Column(t_graphs,widget_row, pn.Row(grids_col, self.dags_col))
+                # self.plot = pn.Row(pn.Column(t_graphs,widget_col, grids_col), self.dags_col)
         else:                
             ## PLOT
             # widget_boxes = self.widget.get_widget_box() 
             if self.status not in ["animated"]:       
-                self.plot = pn.Column(t_graphs,widget_row,pn.Row(pn.Column(grids_col,self.widget.no_i_button),dags_col))        
-                # self.plot = pn.Row(pn.Column(t_graphs,widget_col,t_interaction,grids_col,self.widget.no_i_button), dags_col)
+                self.plot = pn.Column(t_graphs,widget_row,pn.Row(pn.Column(grids_col,self.widget_obj.no_i_button),self.dags_col))        
+                # self.plot = pn.Row(pn.Column(t_graphs,widget_col,t_interaction,grids_col,self.widget.no_i_button), self.dags_col)
             else:        
-                self.plot = pn.Column(t_graphs,widget_row,pn.Row(grids_col,dags_col))       
-                # self.plot = pn.Row(pn.Column(t_graphs,widget_col,t_interaction,grids_col), dags_col)
+                self.plot = pn.Column(t_graphs,widget_row,pn.Row(grids_col,self.dags_col))       
+                # self.plot = pn.Row(pn.Column(t_graphs,widget_col,t_interaction,grids_col), self.dags_col)
     
     def _activate_radio_button_if_single_inter(self, widget):  
         i_type = list(self.action_vars.keys())[0]
@@ -126,5 +128,15 @@ class Demo_Single_Matrix():
         elif i_type == "stratify":
             widget.w_str.value = i_var
 
+    ## GETTERS
     def get_plot(self):
         return self.plot
+
+    def get_first_dag(self):
+        return pn.pane.Bokeh(self.dag_objs[0].get_plot())
+
+    def get_scatter_matrix(self):
+        return self.grid_obj.get_grid()
+
+    def get_scatter_matrix_causal_models(self):
+        return pn.Row(self.grid_obj.get_grid(),self.dags_col)
